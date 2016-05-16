976762dced91ba9bbe097dfe9f190ddfacd629c5
adamShaleen <adamshaleen@gmail.com>
2016-05-16 12:33:20 -0600

    added buttons to toggle admin info

--- public/js/adminController.js
+++ public/js/adminController.js
@@ -1,5 +1,13 @@
 angular.module('dirtWaves').controller('adminController', function($scope, service) {
 
+$scope.admin_products = false;
+
+$scope.toggle_admin_products = function() {
+    $scope.admin_users = false;
+    $scope.admin_orders = false;
+    $scope.admin_products = !$scope.admin_products;
+
+};
 
 $scope.displayProducts = function() {
     service.displayProducts().then(function(response) {
@@ -9,6 +17,17 @@ $scope.displayProducts = function() {
 
 $scope.displayProducts();
 
+//-----------------------------------------------------
+
+
+$scope.admin_users = false;
+
+$scope.toggle_admin_users = function() {
+        $scope.admin_products = false;
+        $scope.admin_orders = false;
+        $scope.admin_users = !$scope.admin_users;
+};
+
 $scope.displayAllUsers = function() {
     service.displayAllUsers().then(function(response) {
         $scope.allUsers = response;
@@ -16,6 +35,17 @@ $scope.displayAllUsers = function() {
 };
 
 $scope.displayAllUsers();
+
+//----------------------------------------------------
+
+
+$scope.admin_orders = false;
+
+$scope.toggle_admin_orders = function() {
+    $scope.admin_products = false;
+    $scope.admin_users = false;
+    $scope.admin_orders = !$scope.admin_orders;
+};
 
 $scope.displayAllOrders = function() {
     service.displayOrder().then(function(response) {
